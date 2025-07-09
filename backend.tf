094c05a75cf31107098ab862e3320eeb65cadeae
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2025-07-09 05:50:25 +0300

    fix

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-018882981263"
+    key            = "lesson-7/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
